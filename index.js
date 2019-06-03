c7a98b150eb9dd0e8247677d74aecb8bcd3de65c
Nate Miller <natekmiller96@gmail.com>
2019-06-03 19:18:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function findMinAndRemove(array){
-
+  let min
 }
 
 function insertionSort(array){
-
+ let newMin = 0;
+ let sorted = [];
+ while (array > 0){
+   newMin = findMinAndRemove(array);
+   sorted.push(newMin);
+ }
+ return sorted;
 }
